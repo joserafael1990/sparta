de97fe616a9224506ea458dcd9bace916764e492
jose <joserafael1990@hotmail.com>
2017-04-18 22:59:51 -0400

    solving issues

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,9 @@ class Event < ApplicationRecord
   	
   	belongs_to :instructor, :class_name => 'Person'
   	belongs_to :host, :class_name => 'Person'
+  	
   	has_many :attends
+  	has_many :people, :through => :attends
 
 	validates :address, :presence => {:message => "Usted ingresar una dirección"}
 	validates :event_date, :presence => {:message => "Usted ingresar una fecha"}
