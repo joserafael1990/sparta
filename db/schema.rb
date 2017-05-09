e5f1cb41737fd5c7c58bba58e8bb3f4b56842598
jose <joserafael1990@hotmail.com>
2017-05-09 18:33:08 -0400

    fixed users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509181459) do
+ActiveRecord::Schema.define(version: 20170509221918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -154,7 +154,7 @@ ActiveRecord::Schema.define(version: 20170509181459) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "active"
-    t.string   "type"
+    t.string   "privilege"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
