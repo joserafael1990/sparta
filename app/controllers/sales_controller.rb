a6b7ff06d4cb1d795c4854b76fcf1414508abf8f
jose <joserafael1990@hotmail.com>
2017-05-12 13:34:41 -0400

    fixing ticket

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -62,7 +62,7 @@ class SalesController < ApplicationController
 		respond_to do |format|
 			format.html
 			format.pdf do
-				render pdf: "factura" , wkhtmltopdf: '/usr/bin/wkhtmltopdf' dísposition: 'inline', :template => 'sales/print.html.erb', :encoding => "utf8"
+				render pdf: "factura" , wkhtmltopdf: '/usr/bin/wkhtmltopdf' , dísposition: 'inline', :template => 'sales/print.html.erb', :encoding => "utf8"
 			end
 		end
 	end
